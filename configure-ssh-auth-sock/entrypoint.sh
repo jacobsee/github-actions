fde7f1b4c94f95aff2fe44641fdca1a8819ba08d
Jacob See <jacob.d.see@gmail.com>
2020-07-20 22:41:57 -0700

    use home environment variable...?

--- configure-ssh-auth-sock/entrypoint.sh
+++ configure-ssh-auth-sock/entrypoint.sh
@@ -4,9 +4,9 @@ DOMAIN=$1
 PRIVATE_KEY=$2
 SSH_AUTH_SOCK=$3
 
-mkdir -p ~/.ssh
+mkdir -p $HOME/.ssh
 echo "Building known_hosts file"
-ssh-keyscan $DOMAIN >> ~/.ssh/known_hosts
+ssh-keyscan $DOMAIN >> $HOME/.ssh/known_hosts
 echo "Built known_hosts file"
 echo "Starting ssh-agent"
 eval `ssh-agent -a $SSH_AUTH_SOCK`
