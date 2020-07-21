50c0c2773582d23c700d531153d930467450cd8e
Jacob See <jacob.d.see@gmail.com>
2020-07-20 22:36:05 -0700

    export environment variable

--- configure-ssh-auth-sock/entrypoint.sh
+++ configure-ssh-auth-sock/entrypoint.sh
@@ -3,7 +3,6 @@
 DOMAIN=$1
 PRIVATE_KEY=$2
 SSH_AUTH_SOCK=$3
-# SSH_AUTH_SOCK=/tmp/ssh_agent.sock
 
 mkdir -p ~/.ssh
 echo "Building known_hosts file"
@@ -13,3 +12,4 @@ echo "Starting ssh-agent"
 eval `ssh-agent -a $SSH_AUTH_SOCK`
 echo "Adding identity"
 ssh-add - <<< $PRIVATE_KEY
+echo "::set-env name=SSH_AUTH_SOCK::$SSH_AUTH_SOCK"
